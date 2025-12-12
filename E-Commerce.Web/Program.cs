454791ec1e41df352e097cbb10c39c3f00cd81c8
Your Name <Nada Elbadry nadaelbadry1012@gmail.com>
2025-12-12 16:08:55 +0200

    programm

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -5,17 +5,30 @@ namespace E_Commerce.Web
     {
         public static void Main(string[] args)
         {
+            #region Create Builder
             var builder = WebApplication.CreateBuilder(args);
+            #endregion
 
+            #region Add Services to Container
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            //builder.Services.AddDbContext<StoreDbContext>(options =>
+            //{
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            //});
 
+
+            #endregion
+
+            #region Build App
+            var app = builder.Build();
+            #endregion
+
+            #region Configure HTTP Request Pipeline
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -27,10 +40,12 @@ namespace E_Commerce.Web
 
             app.UseAuthorization();
 
-
             app.MapControllers();
+            #endregion
 
+            #region Run App
             app.Run();
+            #endregion
         }
     }
 }
